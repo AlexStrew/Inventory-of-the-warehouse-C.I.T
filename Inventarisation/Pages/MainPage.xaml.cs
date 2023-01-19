76631373e7d709df86c43c5e77bd162ea0852c83
Wilastian <sterkhov.sasha@mail.ru>
2023-01-19 14:44:18 +0500

    db filled

--- Inventarisation/Pages/MainPage.xaml.cs
+++ Inventarisation/Pages/MainPage.xaml.cs
@@ -24,5 +24,10 @@ namespace Inventarisation.Pages
         {
             InitializeComponent();
         }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
