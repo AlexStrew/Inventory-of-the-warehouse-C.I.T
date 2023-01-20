79adecb90fbf2a2120913a924d6a2a97794aa2d4
timveur <katya.104.k@gmail.com>
2023-01-20 14:40:21 +0500

    BindingWindow

---
+++ Inventarisation/Views/BindingWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Inventarisation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Inventarisation.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для BindingWindow.xaml
+    /// </summary>
+    public partial class BindingWindow : Window
+    {
+        
+        public BindingWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        
+
+        private void SaveButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
