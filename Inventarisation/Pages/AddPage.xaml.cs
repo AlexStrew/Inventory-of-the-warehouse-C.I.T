be06f0531d7249f07f98237f1f7c632b1b18ed5b
Wilastian <sterkhov.sasha@mail.ru>
2023-01-20 10:42:31 +0500

    Revert "Merge pull request #2 from timveur/Khoroshun"
    This reverts commit 0d46e5d5b0f0fd2626f015b75e7f5f986eabdf6c, reversing
    changes made to c4db9c3525c84eae0170c506b4c14cbe87936f1c.

--- Inventarisation/Pages/AddPage.xaml.cs
+++ Inventarisation/Pages/AddPage.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Configuration;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,19 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Inventarisation.Views
+namespace Inventarisation.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для AddWindow.xaml
+    /// Логика взаимодействия для AddPage.xaml
     /// </summary>
-    public partial class AddWindow : Window
+    public partial class AddPage : Page
     {
-        public AddWindow()
-
+        public AddPage()
         {
-            
             InitializeComponent();
         }
     }
