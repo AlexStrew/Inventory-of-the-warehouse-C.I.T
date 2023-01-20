a1b296a6ffb088eb0ce7a196d3bcd1d193fb3844
timveur <katya.104.k@gmail.com>
2023-01-20 11:46:21 +0500

    nomenclature

---
+++ Inventarisation/Views/NomenclatureWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Inventarisation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Inventarisation.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для NomenclatureWindow.xaml
+    /// </summary>
+    public partial class NomenclatureWindow : Window
+    {
+        Core db = new Core();
+        List<Nomenclature> nomList;
+        public NomenclatureWindow()
+        {
+            InitializeComponent();
+
+            nomList = db.context.Nomenclature.ToList();
+            NomenclatureDG.ItemsSource = nomList;
+        }
+    }
+}
