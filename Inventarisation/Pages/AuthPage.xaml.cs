a6379d37231f8ba50446f5fce548ee3e22c727cf
timveur <katya.104.k@gmail.com>
2023-01-25 10:44:00 +0500

    AuthPage

---
+++ Inventarisation/Pages/AuthPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Inventarisation.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+
+   
+
+        private void LoginBtnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Страница в разработке");
+        }
+
+        private void DontAuthTBMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.NavigationService.Navigate(new MainPage());
+        }
+    }
+}
