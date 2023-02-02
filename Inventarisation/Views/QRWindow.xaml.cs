bff6571f8e1c52a4107da0d03f78ce328575b6b5
seohawkins <horilya2@gmail.com>
2023-02-02 11:27:47 +0500

    QRwin ver.0.1

---
+++ Inventarisation/Views/QRWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using static System.Net.Mime.MediaTypeNames;
+using QRCoder;
+using System.Drawing;
+
+namespace Inventarisation.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для QRAuthWindow.xaml
+    /// </summary>
+    public partial class QRWindow : Window
+    {
+        public QRWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
+            QRCodeData qRCodeData = qRCodeGenerator.CreateQrCode("ТЕСТОВЫЙ ВАРИК", QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qRCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(40);
+            qr.Source = BitmapToImageSource(qrCodeImage);
+        }
+        private ImageSource BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+
+                return bitmapImage;
+            }
+        }
+    }
+}
